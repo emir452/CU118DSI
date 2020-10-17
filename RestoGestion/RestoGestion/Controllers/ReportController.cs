423987b6e3579d18b7167290789653b95c0b2af5
unknown <emir.chesini@gmail.com>
2020-10-16 22:07:28 -0300

    agregada la pantalla seleccionar datos del reporte

--- RestoGestion/RestoGestion/Controllers/ReportController.cs
+++ RestoGestion/RestoGestion/Controllers/ReportController.cs
@@ -1,4 +1,5 @@
-﻿using RestoGestion.Models.Domain;
+﻿using RestoGestion.Models;
+using RestoGestion.Models.Domain;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,8 +15,28 @@ public ActionResult TimesInOrders()
         {
             ViewBag.states =new SelectList(new StateManager().FindAll(), "id_state", "name");
             ViewBag.floors = new SelectList(new FloorManager().FindAll(), "id_floor", "id_floor"); 
+            ViewBag.sectors = new SelectList(new SectorManager().FindAll(), "id_sector", "name");
+            List<string> options = new List<string>();
+            options.Add("Piso");
+            options.Add("Sector");
+            ViewBag.optionsTotalization = new SelectList(options);
             return View();
-
 }
-}
+[HttpPost]
+public ActionResult TimesInOrders(TimesInOrderData requireData)
+        {
+            this.Session["report"] = requireData;
+            return RedirectToAction("SelectVisualizationMode", "Report");
+            }
+[HttpGet]
+        public ActionResult SelectVisualizationMode()
+        {
+List<string> optionsVisualization = new List<string>();
+            optionsVisualization.Add("Por pantalla ");
+            optionsVisualization.Add("PDF");
+            optionsVisualization.Add("Excel");
+            ViewBag.optionsVisualization = new SelectList(optionsVisualization);
+            return View();
+        }
+    }
 }
---
+++ RestoGestion/RestoGestion/Controllers/SectorManager.cs
@@ -0,0 +1,17 @@
+﻿using RestoGestion.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestoGestion.Controllers
+{
+    public class SectorManager
+    {
+        private EntityModel db = new EntityModel();
+        public IList<Sector> FindAll()
+{
+            return db.Sectors.ToList();
+}
+    }
+}
